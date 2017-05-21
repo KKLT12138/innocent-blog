2f39518265a27b32660cc7cc8cfc4af3929017e5
tianzhen <340349237@qq.com>
2017-05-22 00:50:54 +0800

    增加ngAdmin

---
+++ ngHome/src/app/archives/archives.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-archives',
+  templateUrl: './archives.component.html',
+  styleUrls: ['../../public/css/archives.css']
+})
+export class ArchivesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ ngHome/src/app/categories/categories.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-categories',
+  templateUrl: './categories.component.html',
+  styleUrls: ['../../public/css/categories.css']
+})
+export class CategoriesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
