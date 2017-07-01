bc566f10f952b27bd8cc1c24d68bb738419ca580
tianzhen <340349237@qq.com>
2017-07-01 15:11:24 +0800

    增加字符串常量配置

--- api/login.js
+++ api/login.js
@@ -1,11 +1,21 @@
 var express = require('express');
 var router = express.Router();
 var AuserModel = require('../models/auser');
+var lang = require('../lib/lang.json');
 
 router.route('/login')
+    /*
+    * 后台管理登录页
+    * /admin/login
+    * */
     .get(function (req, res, next) {
         res.render('admin/login');
     })
+    /*
+    * 登录接口
+    * demo：
+    *
+    * */
     .post(function (req, res, next) {
         var username = req.body.username;
         var password = req.body.password;
@@ -15,10 +25,27 @@ router.route('/login')
         auserQuery.exec(function (err, ausers) {
             ausers.forEach(function (auser, index) {
                 console.log(auser.name)
+                if (username === auser.name) {
+                    if (password === auser.password) {
+                        res.json(200, {
+                            status: 1,
+                            message: lang.success
+                        });
+                    } else {
+                        res.json(200, {
+                            status: 0,
+                            message: lang.passwordError
+                        });
+                    }
+                } else {
+                    res.json(200, {
+                        status: 1,
+                        message: lang.usernameError
+                    });
+                }
             })
-        })
-        res.json(200, {"word": "hello"});
-    })
+        });
 
+    })
 
 module.exports = router;
