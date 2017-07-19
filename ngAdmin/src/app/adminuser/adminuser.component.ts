5009c91a0d11d27003a7c3d4b5a3499f8287be7a
tianzhen <340349237@qq.com>
2017-07-19 19:20:49 +0800

    修复后台管理员列表获取功能

--- ngAdmin/src/app/adminuser/adminuser.component.ts
+++ ngAdmin/src/app/adminuser/adminuser.component.ts
@@ -1,6 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 
 import { AdminuserService } from './adminuser.service';
+import { Config } from '../share/config';
+import { LoadingAnimateComponent } from '../loading-animate/loading-animate.component';
+import { MessageDialogComponent } from '../message-dialog/message-dialog.component';
 
 @Component({
   selector: 'admin-user',
@@ -14,7 +17,18 @@ import { AdminuserService } from './adminuser.service';
   ]
 })
 export class AdminUserComponent implements OnInit {
-  adminusers: any = [{}];
+  @ViewChild(LoadingAnimateComponent) loadingAnimateComponent: LoadingAnimateComponent;
+  @ViewChild(MessageDialogComponent) messageDialogComponent: MessageDialogComponent;
+
+  adminusers: any = [];
+
+  mask = {
+    display: true
+  };
+
+  modal = {
+    display: false,
+  };
 
   constructor(
     private _adminuserService: AdminuserService
@@ -28,12 +42,17 @@ export class AdminUserComponent implements OnInit {
     return this._adminuserService.getAdminusers()
       .subscribe(datas => {
         datas.forEach( (data, index) => {
+          this.adminusers[index] = {};
           this.adminusers[index].id = data.id;
           this.adminusers[index].name = data.name;
           this.adminusers[index].ip = data.ip;
           this.adminusers[index].date = data.date;
           this.adminusers[index].createDate = data.createDate;
         })
+      }, error => {
+        this.mask.display = false;
+        this.loadingAnimateComponent.loading.display = false;
+        this.messageDialogComponent.messageDialog.open(Config.message.getError, 0);
       });
   }
 
